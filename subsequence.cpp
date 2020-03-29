afc5044f834a4d166a379fd2ded8a862e047a23e
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-03-29 14:01:17 +0530

    return subsequence of a string

---
+++ subsequence.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sub(string input,string output[])
+{
+    if(input.empty())
+    {
+    output[0]=" ";
+    return 1;
+    }
+    string subs =input.substr(1);
+     int smalloutputsize =sub(subs,output);
+
+    for(int i=0;i<smalloutputsize;i++)
+    {
+        output[i+smalloutputsize]=input[0]+output[i];
+
+    }
+    return 2*smalloutputsize;
+
+
+
+}
+
+int main()
+{
+string input;
+cin>>input;
+int s=input.size();
+s=pow(2,s);
+string *output= new string[s];
+
+sub(input,output);
+for(int i=0;i<s;i++)
+    cout<<output[i]<<endl;
+return 0;
+}
